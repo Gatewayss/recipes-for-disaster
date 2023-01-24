03b305f39ab2b814b6fbdcf6f6a7cee95d19c408
Gage Norton <nortongage0@gmail.com>
2023-01-23 21:10:59 -0600

    Add data function for food input

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-/* first comment */
+let ingredient = "egg"
+let foodURL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + ingredient
+const foodContainer = document.getElementById('food')
+
+requestData(foodURL)
+function requestData(foodURL) {
+    fetch(foodURL)
+        .then((response) => response.json())
+        .then((data) => displayFoodData(data));
+}
+
+function displayFoodData(data) {
+    console.log(data.meals[0].strMeal);
+    console.log(data.meals[0].strMealThumb)
+}
+
+
