35969196cf07b3fcf87746530226e4f1b1882bee
MUNTAAS <98139114+Mhassan0630@users.noreply.github.com>
2023-01-24 20:55:45 -0600

    Fixed the ID's iin Html and Main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,35 @@
-const foodInput = document.getElementById('foodInput')
-const foodSearchBtn = document.getElementById('searchBtn')
-const foodContainer = document.getElementById('food')
-const drinkContainer =document.getElementById('drink')
+const foodInput = document.getElementById('foodInput');
+const foodSearchBtn = document.getElementById('foodSearchBtn');
+const foodContainer = document.getElementById('food');
+const drinkInput = document.getElementById('drinkInput');
+const drinkSearchBtn = document.getElementById('drinkSearchBtn');
+const drinkContainer =document.getElementById('drink');
 
-let spirit = "gin"
-let drinkURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + spirit
+drinkSearchBtn.addEventListener('click', function () {
+    let spirit = drinkInput.value;
+    let drinkURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + spirit;
+    requestDrinkData(drinkURL);
+});
 
+function requestDrinkData(drinkURL) {
+    fetch(drinkURL)
+        .then((response) => response.json())
+        .then((data) => displayDrinkData(data))
+        .catch(error => console.error('Error:', error));
+}
 
-searchBtn.addEventListener('click', function () {
-    let ingredient = foodInput.value
-    let foodURL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + ingredient
-    requestData(foodURL)
-})
+function displayDrinkData(data) {
+    console.log(data.drinks[0].strDrink);
+    console.log(data.drinks[0].strDrinkThumb)
+}
 
-function requestData(foodURL) {
+foodSearchBtn.addEventListener('click', function () {
+    let ingredient = foodInput.value;
+    let foodURL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + ingredient;
+    requestFoodData(foodURL);
+});
+
+function requestFoodData(foodURL) {
     fetch(foodURL)
         .then((response) => response.json())
         .then((data) => displayFoodData(data));
@@ -23,17 +39,3 @@ function displayFoodData(data) {
     console.log(data.meals[0].strMeal);
     console.log(data.meals[0].strMealThumb)
 }
-
-/*
-
-function requestData(drinkURL) {
-    fetch(drinkURL)
-        .then((response) => response.json())
-        .then((data) => displayDrinkData(data));
-}
-
-function displayDrinkData(data) {
-    console.log(data.drinks[0].strDrink);
-    console.log(data.drinks[0].strDrinkThumb)
-}
-*/
