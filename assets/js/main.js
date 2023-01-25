9c4b6a8df73dc1635daabc017145db1df69fab4b
apersonius <personius12@gmail.com>
2023-01-24 20:48:06 -0600

    create drink event fn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,31 @@
 const foodInput = document.getElementById('foodInput')
-const foodSearchBtn = document.getElementById('searchBtn')
+const foodSearchBtn = document.getElementById('foodSearchBtn')
 const foodContainer = document.getElementById('food')
+const drinkInput = document.getElementById('drinkInput')
+const drinkSearchBtn = document.getElementById('drinkSearchBtn')
 const drinkContainer =document.getElementById('drink')
 
-let spirit = "gin"
-let drinkURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + spirit
+//let spirit = "gin"
+//let drinkURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + spirit
 
+drinkSearchBtn.addEventListener('click', function () {
+    let spirit = drinkInput.value
+    let drinkURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + spirit
+    requestDrinkData(drinkURL)
+})
 
-searchBtn.addEventListener('click', function () {
+function requestDrinkData(drinkURL) {
+    fetch(drinkURL)
+        .then((response) => response.json())
+        .then((data) => displayDrinkData(data));
+}
+
+function displayDrinkData(data) {
+    console.log(data.drinks[0].strDrink);
+    console.log(data.drinks[0].strDrinkThumb)
+}
+
+foodSearchBtn.addEventListener('click', function () {
     let ingredient = foodInput.value
     let foodURL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + ingredient
     requestData(foodURL)
@@ -23,17 +41,3 @@ function displayFoodData(data) {
     console.log(data.meals[0].strMeal);
     console.log(data.meals[0].strMealThumb)
 }
-
-/*
-
-function requestData(drinkURL) {
-    fetch(drinkURL)
-        .then((response) => response.json())
-        .then((data) => displayDrinkData(data));
-}
-
-function displayDrinkData(data) {
-    console.log(data.drinks[0].strDrink);
-    console.log(data.drinks[0].strDrinkThumb)
-}
-*/
